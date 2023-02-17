010a9287ad53e858e96912481028eca46781f27b
heeyeon3050 <111184269+heeyeon3050@users.noreply.github.com>
2023-02-17 17:20:49 +0900

    update

--- src/main/java/codingrecipe/member/dto/MemberDTO.java
+++ src/main/java/codingrecipe/member/dto/MemberDTO.java
@@ -1,10 +1,13 @@
 package codingrecipe.member.dto;
 
+import codingrecipe.member.entity.MemberEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Member;
 
 @Getter
 @Setter
@@ -15,4 +18,13 @@ public class MemberDTO {
     private String memberEmail;
     private String memberPassword;
     private String memberName;
+
+    public static MemberDTO toMemberDTO(MemberEntity memberEntity){
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setId(memberEntity.getId());
+        memberDTO.setMemberEmail(memberEntity.getMemberEmail());
+        memberDTO.setMemberPassword(memberEntity.getMemberPassword());
+        memberDTO.setMemberName(memberEntity.getMemberName());
+        return memberDTO;
+    }
 }
